cad6060e363479fde9325cc22ad05ebae37b6a91
Subhayan Bhattacharya <susubhat@oradev.oraclecorp.com>
2018-07-30 00:13:18 +0530

    *

---
+++ DS and algorithmic thinking with Python/Recursion/generate-all-strings-of-length.py
@@ -0,0 +1,24 @@
+# This is not exactly recursion
+
+# http://code.activestate.com/recipes/425303-generating-all-strings-of-some-length-of-a-given-a/
+
+
+def allstrings(alphabet, length):
+	"""Find the list of all strings of 'alphabet' of length 'length'"""
+	
+	if length == 0: return []
+	
+	c = [[a] for a in alphabet[:]] # Dont think we need this at all
+	if length == 1: return c
+	
+	c = [[x,y] for x in alphabet for y in alphabet]
+	if length == 2: return c
+	
+	for l in range(2, length):
+		c = [[x]+y for x in alphabet for y in c]
+		
+	return c
+
+if __name__ == "__main__":
+	for p in allstrings([0,1,2],4):
+		print (p)
