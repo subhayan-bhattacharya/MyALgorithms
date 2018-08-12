c4c921c99928ada7137128f6154f521fd6dbaa14
Subhayan Bhattacharya <susubhat@oradev.oraclecorp.com>
2018-08-12 14:31:44 +0530

    Changed the football fest code a bit

--- hackerearth/the-football-fest.py
+++ hackerearth/the-football-fest.py
@@ -1,26 +1,26 @@
 # https://www.hackerearth.com/challenge/hiring/superprofs-hiring-challenge/algorithm/the-football-fest-6/
-
 test_cases = int(input())
 players = []
 for test in range(test_cases):
     first,second = input().split(' ')
+    # Initially the player second has the ball
     players.append(second)
     back_flag = False
-    for j in range(int(first)):
-        checks = input().split(' ')
-        if len(checks) > 1:
-            operation,player = checks[0],checks[1]
-            players.append(player)
+    # This is to keep track of the last player who had the ball when there was a back attempted otherwise we would not be able to track when the
+    # player passed the ball back
+    temp = None
+    for i in range(int(first)):
+        input_values = input().split(' ')
+        if len(input_values) > 1:
+            operation, player_id = input_values[0], input_values[1]
+            players.append(player_id)
             if back_flag:
                 back_flag = False
         else:
-            if back_flag:
-                temp = players[-1]
-                players.append(last_player)
-                last_player = temp
-            else:
-                last_player = players[-1]
-                del players[-1]
+            if not back_flag:
+                temp = players.pop()
                 back_flag = True
-
-    print("Player {}".format(str(players[-1])))
+            else:
+                players.append(temp)
+                back_flag = False
+    print("Player %s" % players[-1])
